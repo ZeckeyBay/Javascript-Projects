509346eff4ffe790c18813a0983a25f74d484561
ZeckeyBay <zfatihturan@gmail.com>
2022-09-10 22:05:58 +0300

    Times and Notifications added

--- Push Notifications JS/script.js
+++ Push Notifications JS/script.js
@@ -1,5 +1,35 @@
 const button = document.querySelector("button")
 
 button.addEventListener("click",() => {
-    alert("Selam")
+    Notification.requestPermission().then(perm => {
+        if (perm == "granted"){
+            const notification = new Notification("Örnek Bildirim",{
+                body:"Daha fazla yazı",
+                data: {hello: "world"},
+                tag : "Welcome message",
+            })
+            notification.addEventListener("click",e =>{
+                console.log(e)
+            })
+        }
+    })
+})
+let notification
+let interval
+document.addEventListener("visibilitychange",() =>{
+    if (document.visibilityState==="hidden"){
+        const leaveDate = new Date()
+        setInterval(() => {
+            notification = new Notification("Geri gel lütfen",{
+                body:`${Math.round((new Date() - leaveDate) / 1000)} saniyedir yoksun`,
+                tag:"Geri gel",
+            })
+        },100)
+        notification = new Notification("Geri gel lütfen",{
+            body:"Lütfeenn",
+            tag:"Geri gel",
+        })
+    } else {
+        notification.close()
+    }
 })
