8fe823f5fb1241df84f870a3f25b8d3cf9ee7cb2
ZeckeyBay <zfatihturan@gmail.com>
2022-09-10 14:36:38 +0300

    comments added

--- Exercises/exercise1.js
+++ Exercises/exercise1.js
@@ -1,4 +1,8 @@
-//exercise1
+//exercise1   An isogram is a word that has no repeating letters, consecutive or non-consecutive. 
+//Example: (Input --> Output)
+//"Dermatoglyphics" --> true
+//"aba" --> false
+//"moOse" --> false (ignore letter case)
 function isIsogram(str){
   str = str.toLowerCase();
   for(let i = 0; i < str.length; ++i)
@@ -8,7 +12,8 @@ function isIsogram(str){
   return true;
 }
 
-//exercise2
+//exercise2 allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
+//If the function is passed a valid PIN string, return true, else return false.
 function validatePIN (pin) {
   if (/[a-z]/i.test(pin)){
       return false;
@@ -18,7 +23,7 @@ function validatePIN (pin) {
     return true;
   }
 }
-//exercise2.1
+//exercise2.1  One line advanced version
 function validatePIN(pin) {
   return /^(\d{4}|\d{6})$/.test(pin)
 }
