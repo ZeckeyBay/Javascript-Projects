ba5a39cd9dadb4400985cf4971a534e5c6cbf8fb
ZeckeyBay <zfatihturan@gmail.com>
2022-09-10 14:50:41 +0300

    answers added

--- Exercises/exerciseset1.js
+++ Exercises/exerciseset1.js
@@ -11,6 +11,8 @@ function isIsogram(str){
         return false;
   return true;
 }
+console.log(isIsogram("computer"))
+
 
 //exercise2 allow 4 or 6 digit PIN codes and PIN codes cannot contain anything but exactly 4 digits or exactly 6 digits.
 //If the function is passed a valid PIN string, return true, else return false.
@@ -27,3 +29,15 @@ function validatePIN (pin) {
 function validatePIN(pin) {
   return /^(\d{4}|\d{6})$/.test(pin)
 }
+console.log(validatePIN(12312312))
+
+
+
+//exercise 3 Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+//Example(Input => Output):
+//35231 => [1,3,2,5,3]    0 => [0]
+
+function digitize(n) {
+  return String(n).split('').map(Number).reverse()
+}
+console.log(digitize(54321))
