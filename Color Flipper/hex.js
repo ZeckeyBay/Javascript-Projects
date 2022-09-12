c9a011fbf18e0466a834f5ff19808973c02cf1d8
ZeckeyBay <zfatihturan@gmail.com>
2022-09-12 13:41:49 +0300

    done for now but css is a bit messy

--- Color Flipper/hex.js
+++ Color Flipper/hex.js
@@ -13,6 +13,6 @@ btn.addEventListener("click", function () {
   document.body.style.backgroundColor = hexColor;
 });
 
-getRandomNumber = () => {
+getRandomNumber = () =>{
   return Math.floor(Math.random() * hex.length);
 }
