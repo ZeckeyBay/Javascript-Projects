63397ea1507cdab3fa76fc3abc046e16c3bb783a
ZeckeyBay <zfatihturan@gmail.com>
2022-09-24 18:21:37 +0300

    ex 3

--- Exercises/exerciseset2.js
+++ Exercises/exerciseset2.js
@@ -34,4 +34,23 @@ setTimeout(() => {
 child.addEventListener("click", e=> {
     console.log("Child Bubble")
 })
+
+//exercise 3 : query create 
+const divs = document.querySelectorAll("div")
+
+addGlobalEventListener("click","div",e => {
+    console.log("hi")
+})
+
+function addGlobalEventListener(type, selector, callback) {
+    document.addEventListener(type, e=>{
+        if (e.target.matches(selector)) callback(e)
+    })
+}
+
+const newDiv = document.createElement("div")
+newDiv.style.width = "200px"
+newDiv.style.height = "200px"
+newDiv.style.backgroundColor= "purple"
+document.body.append(newDiv)
  
