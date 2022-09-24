a2546a50e30f25c56e7bef2f93b4e5c3d3ddbf26
ZeckeyBay <zfatihturan@gmail.com>
2022-09-24 17:56:14 +0300

    eventlisteners

--- Exercises/exerciseset2.js
+++ Exercises/exerciseset2.js
@@ -6,3 +6,32 @@ let isPositive = number => number >= 0
 let randomNumber = () => Math.random
 
 document.addEventListener("click", () => console.log("click"))
+
+//exercise 2 :
+
+const grandparent = document.querySelector(".grandparent")
+const parent = document.querySelector(".parent")
+const child = document.querySelector(".child")
+
+grandparent.addEventListener("click", e => {
+    console.log("Grandparent 1 ")
+})
+
+parent.addEventListener("click", e => {
+    console.log("Parent 1 ")
+})
+
+document.addEventListener("click", e => {
+    console.log("Document 1 ")
+})
+
+setTimeout(() => {
+    parent.removeEventListener("click", () => {
+        console.log("hi")
+    })
+}, 2000)
+
+child.addEventListener("click", e=> {
+    console.log("Child Bubble")
+})
+ 
