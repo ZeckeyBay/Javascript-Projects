dd89d2b330dfc4a83ea7a55ae3a8ff3d655df9e8
ZeckeyBay <zfatihturan@gmail.com>
2022-09-24 16:50:15 +0300

    arrow func

--- Exercises/exerciseset2.js
+++ Exercises/exerciseset2.js
@@ -1,1 +1,8 @@
 //exercise 1 : 
+let summ = (a,b) => a + b
+
+let isPositive = number => number >= 0
+
+let randomNumber = () => Math.random
+
+document.addEventListener("click", () => console.log("click"))
