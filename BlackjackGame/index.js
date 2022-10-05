627b7329fa9ade91464cdd80fc346953324282c6
ZeckeyBay <zfatihturan@gmail.com>
2022-10-05 12:41:49 +0300

    random card generator

--- BlackjackGame/index.js
+++ BlackjackGame/index.js
@@ -1,8 +1,6 @@
 
-let firstCard = 10
-let secondCard = 10
-let cards = [firstCard,secondCard]
-let sum = firstCard + secondCard
+let cards = [];
+let sum = 0;
 let hasBlackJack = false
 let isAlive = true
 let message = ""
@@ -10,7 +8,23 @@ let messageEl = document.getElementById("message-el")
 let sumEl = document.querySelector("#sum-el")
 let cardsEl = document.getElementById("cards-el")
 
+function getRandomCard() {
+    let randomNumber = Math.floor(Math.random()*11) + 1 ;
+    if (randomNumber > 10){
+        return 10;
+    } else if (randomNumber === 1){
+        return 11;
+    } else {
+        return randomNumber
+    }
+}
+
 function startGame(){
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard,secondCard];
+    sum = firstCard + secondCard;
+    isAlive = true;
     renderGame(); 
 }
 function renderGame(){
@@ -34,7 +48,7 @@ function renderGame(){
 }
 
 function newCard(){
-    let card = 3;
+    let card = getRandomCard();
     sum += card;
     cards.push(card);
     renderGame();
