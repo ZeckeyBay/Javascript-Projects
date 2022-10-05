c82429117e84ecb87fef4ff32af348d28174f8a8
ZeckeyBay <zfatihturan@gmail.com>
2022-10-05 11:21:24 +0300

    cards and sum displayed

--- BlackjackGame/index.js
+++ BlackjackGame/index.js
@@ -6,8 +6,12 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.querySelector("#sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
     if (sum <= 20) {
         message = "Do you want to draw a new card ? "
     } else if (sum === 21) {
