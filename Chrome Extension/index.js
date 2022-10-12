4c7899232a58ee672c5090af2e2f52c6eb01c3ff
ZeckeyBay <zfatihturan@gmail.com>
2022-10-12 17:56:21 +0300

    save button now works

--- Chrome Extension/index.js
+++ Chrome Extension/index.js
@@ -2,12 +2,22 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputbtn = document.getElementById("input-btn")
-const ulEl = document.getElementById("el-el");
+const ulEl = document.getElementById("ul-el");
 
 inputbtn.addEventListener("click",function(){
     myLeads.push(inputEl.value)
+    inputEl.value = ""
+    renderLeads();
 })
 
-for (let i=0; i<myLeads.length;i++){
-    ulEl.textContent = myLeads[i] + " "
+
+function renderLeads(){
+let listItems = ""
+    for (let i=0; i<myLeads.length;i++){
+    listItems = document.createElement("li")
+    listItems.textContent = myLeads[i]
+    
 }
+ulEl.append(listItems)
+}
+
