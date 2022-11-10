7674ce43f9350db0fe76872b6ed6e4f5250b72e7
ZeckeyBay <zfatihturan@gmail.com>
2022-11-10 13:33:00 +0300

    am pm correction

--- Digital Clock/index.js
+++ Digital Clock/index.js
@@ -13,6 +13,13 @@ if(h>12){
     h = h -12
     ampm = "PM"
 }
+
+h= h<10 ? "0" + h:h;
+m= m<10 ? "0" + m:m;
+s= s<10 ? "0" + s:s;
+
+
+
 hourEl.innerText = h;
 minEl.innerText = m;
 secEl.innerText = s;
