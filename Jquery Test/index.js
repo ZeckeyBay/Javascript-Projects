89c83b65b26d6f1318ae57ecb8a4c661a1501e8e
ZeckeyBay <zfatihturan@gmail.com>
2022-11-28 12:13:28 +0300

    Jquery fade slide test

---
+++ Jquery Test/index.js
@@ -0,0 +1,30 @@
+$(function(){
+    $("#btn-fadein").click(function(){
+        $(".box-red").fadeIn(3000);
+        $(".box-blue").fadeIn(2000);
+        $(".box-green").fadeIn(4000);
+    })
+    $("#btn-fadeout").click(function(){
+        $(".box-red").fadeOut(3000);
+        $(".box-blue").fadeOut(2000);
+        $(".box-green").fadeOut(4000);
+    })
+    $("#btn-fadetoggle").click(function(){
+        $(".box-red").fadeToggle(3000);
+        $(".box-blue").fadeToggle(2000);
+        $(".box-green").fadeToggle(4000);
+    })
+})
+
+$(function(){
+    $("#slide-down").click(function(){
+        $(".slide-para").slideDown(1500);
+    })
+    $("#slide-up").click(function(){
+        $(".slide-para").slideUp(1500);
+    })
+    $("#slide-toggle").click(function(){
+        $(".slide-para").slideToggle(1500);
+    })
+})
+
